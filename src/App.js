a7072fff4f5679ceda524267651c314e77b99198
Jędrzej Rzankowski <49168269+JRzankowski@users.noreply.github.com>
2020-02-11 11:32:44 +0100

    routing, file structure ready

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Home from "./components/Home";
+import './scss/main.scss'
+
+
+
+function App() {
+  return (
+    <div className="App">
+        <Router>
+            <div>
+                <Route exact path="/">
+                    <Home/>
+                </Route>
+            </div>
+        </Router>
+    </div>
+  );
+}
+
+export default App;
