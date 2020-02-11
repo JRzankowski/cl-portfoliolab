aa457771ba5c43ddbb46f35a83766f72cd92a85c
Jędrzej Rzankowski <49168269+JRzankowski@users.noreply.github.com>
2020-02-11 16:12:02 +0100

    mainSection ready

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,16 @@
 import React, {Component} from 'react';
 import HomeHeader from "./HomeHeader/HomeHeader";
+import HomeMainSection from "./HomeMainSection";
 
 export default class Home extends Component {
     render() {
         return (
-            <HomeHeader/>
+
+            <div className="container">
+                <HomeHeader/>
+                <HomeMainSection/>
+
+            </div>
         );
     }
 }
