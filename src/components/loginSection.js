45c60bd3410426b4d164c6cb0f2eb4f90ef663a5
Jędrzej Rzankowski <49168269+JRzankowski@users.noreply.github.com>
2020-02-18 15:58:21 +0100

    login and register validation ready

--- src/components/loginSection.js
+++ src/components/loginSection.js
@@ -3,6 +3,40 @@ import HomeHeader from "./HomeHeader/HomeHeader";
 import {Link} from "react-router-dom";
 
 export default class Login extends Component {
+
+
+    handleInput = e => {
+        let regex = null;
+        const input_regex = {
+            email: /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[A-Za-zżźćńółęąśŻŹĆĄŚĘŁÓŃ]{2,}))$/,
+        };
+        if (e.target.classList.contains('email')) {
+            regex = input_regex.email;
+        }
+        if (e.target.classList.contains('password')) {
+            if ((e.target.value.length) > 6) {
+                e.target.classList.remove('invalid');
+                e.target.classList.add('valid')
+            } else {
+                e.target.classList.remove('valid');
+                e.target.classList.add('invalid');
+            }
+        }
+        if (regex) {
+            if (regex.test(e.target.value)) {
+                e.target.classList.remove('invalid');
+                e.target.classList.add('valid')
+
+            } else {
+                e.target.classList.remove('valid');
+                e.target.classList.add('invalid');
+            }
+        }
+        if (e.target.value.length === 0) {
+            e.target.classList.remove('valid', 'invalid');
+        }
+    };
+
     render() {
 
         return (
@@ -15,10 +49,14 @@ export default class Login extends Component {
                     </div>
                     <div className="log-in-box__panel">
                         <label className='log-in-box__panel--label' htmlFor='email'>Email
-                            <input className='log-in-box__panel--input' type="email" id="email"/>
+                            <input onInput={this.handleInput} className='log-in-box__panel--input email' type="email"
+                                   id="email"/>
+                            <p>Podany email jest nieprawidłowy!</p>
                         </label>
                         <label className='log-in-box__panel--label' htmlFor='password'>Hasło
-                            <input className='log-in-box__panel--input' type="password" id="password"/>
+                            <input onInput={this.handleInput} className='log-in-box__panel--input password'
+                                   type="password" id="password"/>
+                            <p>Podane hasło jest za krótkie!</p>
                         </label>
                     </div>
                     <Link className='log-in-box__button' to='/register'>Załóż konto</Link>
--- src/components/registerSection.js
+++ src/components/registerSection.js
@@ -3,6 +3,65 @@ import HomeHeader from "./HomeHeader/HomeHeader";
 import {Link} from "react-router-dom";
 
 export default class Register extends Component {
+    state = {
+        passwordCheck: false
+    };
+    handleInput = e => {
+        let regex = null;
+        const input_regex = {
+            email: /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[A-Za-zżźćńółęąśŻŹĆĄŚĘŁÓŃ]{2,}))$/,
+        };
+        if (e.target.classList.contains('email')) {
+            regex = input_regex.email;
+        }
+        if (e.target.classList.contains('password')) {
+            if ((e.target.value.length) >= 6) {
+                e.target.classList.remove('invalid');
+                e.target.classList.add('valid');
+                this.setState({
+                    passwordCheck: true
+                })
+            } else {
+                e.target.classList.remove('valid');
+                e.target.classList.add('invalid');
+                document.querySelector('.register-box__panel--input.repeat-password').disabled = true;
+                this.setState({
+                    passwordCheck: false
+                })
+            }
+        }
+        if (this.state.passwordCheck) {
+            document.querySelector('.register-box__panel--input.repeat-password').removeAttribute('disabled');
+        } else {
+            document.querySelector('.register-box__panel--input.repeat-password').disabled = true;
+            document.querySelector('.register-box__panel--input.repeat-password').value = "";
+            document.querySelector('.register-box__panel--input.repeat-password').classList.remove('invalid');
+            document.querySelector('.register-box__panel--input.repeat-password').classList.remove('valid');
+        }
+        if (e.target.classList.contains('repeat-password')) {
+            if (e.target.value === document.querySelector('.register-box__panel--input.password').value) {
+                e.target.classList.remove('invalid');
+                e.target.classList.add('valid');
+            } else {
+                e.target.classList.remove('valid');
+                e.target.classList.add('invalid');
+            }
+        }
+        if (regex) {
+            if (regex.test(e.target.value)) {
+                e.target.classList.remove('invalid');
+                e.target.classList.add('valid')
+
+            } else {
+                e.target.classList.remove('valid');
+                e.target.classList.add('invalid');
+            }
+        }
+        if (e.target.value.length === 0) {
+            e.target.classList.remove('valid', 'invalid');
+        }
+    };
+
     render() {
 
         return (
@@ -15,16 +74,23 @@ export default class Register extends Component {
                     </div>
                     <div className="register-box__panel">
                         <label className='register-box__panel--label' htmlFor='email'>Email
-                            <input className='register-box__panel--input' type="email" id="email"/>
+                            <input onInput={this.handleInput} className='register-box__panel--input email' type="email"
+                                   id="email"/>
+                            <p>Podany email jest nieprawidłowy!</p>
                         </label>
-                        <label className='register-box__panel--label' htmlFor='password'>Hasło
-                            <input className='register-box__panel--input' type="password" id="password"/>
+                        <label className='register-box__panel--label ' htmlFor='password'>Hasło
+                            <input onInput={this.handleInput} className='register-box__panel--input password'
+                                   type="password" id="password"/>
+                            <p>Podane hasło jest za krótkie!</p>
                         </label>
-                        <label className='register-box__panel--label' htmlFor='password'>Powtórz hasło
-                            <input className='register-box__panel--input' type="password" id="password"/>
+                        <label className='register-box__panel--label ' htmlFor='password'>Powtórz hasło
+                            <input disabled onInput={this.handleInput}
+                                   className='register-box__panel--input repeat-password'
+                                   type="password" id="password"/>
+                            <p>Podane hasło nie jest takie samo!</p>
                         </label>
                     </div>
-                    <Link className='register-box__button' to='/register'>Zaloguj się</Link>
+                    <Link className='register-box__button' to='/login'>Zaloguj się</Link>
                     <Link className='register-box__button' to='/register' type='submit'>Załóż konto</Link>
                 </div>
             </section>
