389226e07ea99dfe7ecf326a48b82662dcae3f0d
Jędrzej Rzankowski <49168269+JRzankowski@users.noreply.github.com>
2020-02-18 13:42:51 +0100

    fetch ready

--- src/components/contactUsSection.js
+++ src/components/contactUsSection.js
@@ -4,18 +4,32 @@ import Footer from "./Footer";
 export default class ContactUs extends Component {
 
 
-    handleInput(e) {
-        console.log(e.target);
+    state = {
+        name: '',
+        email: '',
+        message: '',
+    };
+
+    handleInput = e => {
         let regex = null;
-        const input_fields = {
+        const input_regex = {
             username: /^[A-Za-zżźćńółęąśŻŹĆĄŚĘŁÓŃ]*$/,
             email: /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[A-Za-zżźćńółęąśŻŹĆĄŚĘŁÓŃ]{2,}))$/,
         };
         if (e.target.classList.contains('name')) {
-            regex = input_fields.username
+            regex = input_regex.username;
+            this.setState({
+                name: e.target.value
+            })
         } else if (e.target.classList.contains('email')) {
-            regex = input_fields.email;
+            regex = input_regex.email;
+            this.setState({
+                email: e.target.value
+            })
         } else if (e.target.classList.contains('text-area')) {
+            this.setState({
+                message: e.target.value
+            });
             if ((e.target.value.length) > 120) {
                 e.target.classList.remove('invalid');
                 e.target.classList.add('valid')
@@ -24,11 +38,11 @@ export default class ContactUs extends Component {
                 e.target.classList.add('invalid');
             }
         }
-        console.log(e.target.value.length);
         if (regex) {
             if (regex.test(e.target.value)) {
                 e.target.classList.remove('invalid');
                 e.target.classList.add('valid')
+
             } else {
                 e.target.classList.remove('valid');
                 e.target.classList.add('invalid');
@@ -37,7 +51,36 @@ export default class ContactUs extends Component {
         if (e.target.value.length === 0) {
             e.target.classList.remove('valid', 'invalid');
         }
-    }
+
+    };
+
+    handleSubmit = e => {
+        e.preventDefault();
+        const url = `https://fer-api.coderslab.pl/v1/portfolio/contact`;
+        const data = {name: this.state.name, email: this.state.email, message: this.state.message};
+        fetch(url, {
+            method: 'POST',
+            body: JSON.stringify(data),
+            headers: {
+                "Content-Type": "application/json"
+            }
+        })
+            .then(res => res.json())
+            .then(response => {
+                    if (response.status === 'success') {
+                        document.querySelector('.contact__form-message.error').classList.remove('active');
+                        document.querySelector('.contact__form-message.success').classList.add('active')
+                        for(let el of document.querySelectorAll('.input-validation')){
+                            el.value = "";
+                        }
+                    } else {
+                        document.querySelector('.contact__form-message.success').classList.remove('active');
+                        document.querySelector('.contact__form-message.error').classList.add('active');
+
+                    }
+                }
+            )
+    };
 
     render() {
         return (
@@ -48,24 +91,27 @@ export default class ContactUs extends Component {
                         <div className="contact__form--container">
                             <h2 className="contact__form-heading">Skontaktuj się z nami</h2>
                             <div className="decoration"/>
-                            <form className="form">
+                            <p className='contact__form-message success'>Wiadomość została wysłana! <br/> Wkrótce się
+                                skontaktujemy</p>
+                            <p className='contact__form-message error'>Coś poszło nie tak ...</p>
+                            <form className="form" onSubmit={this.handleSubmit}>
                                 <div className="form__inputs">
                                     <label className='form__inputs--label' htmlFor="name">Wpisz swoje imię:
                                         <input onInput={this.handleInput} placeholder='Krzysztof'
-                                               className='form__inputs--input name' type="text" id="name"
+                                               className='form__inputs--input name input-validation' type="text" id="name"
                                                name="name"/>
                                         <p>Podane imię jest nieprawidłowe !</p>
                                     </label>
                                     <label className='form__inputs--label' htmlFor="email">Wpisz swój email:
                                         <input onInput={this.handleInput} placeholder='abc@xyz.pl'
-                                               className='form__inputs--input email'
+                                               className='form__inputs--input email input-validation'
                                                type="email"
                                                id="email" name="email"/>
                                         <p>Podany email jest nieprawidłowy !</p>
                                     </label>
                                 </div>
                                 <label className='form__text-area-label' htmlFor="message">Wpisz swoją wiadomość:
-                                    <textarea onInput={this.handleInput} className='form__text-area text-area'
+                                    <textarea onInput={this.handleInput} className='form__text-area text-area input-validation'
                                               id="message"
                                               rows="4"
                                               cols="50"
